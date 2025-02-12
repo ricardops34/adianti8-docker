c789df44b044784451b1066e5dc133a95b76386a
Leandro Ferreira da Luz <ferreiralea@sap.sp.gov.br>
2025-02-12 16:13:47 -0300

    aponta arquivos de configuração para servidor de banco de dados postgresql host db do docker

--- app/config/unit_a.php
+++ app/config/unit_a.php
@@ -1,10 +1,19 @@
 <?php
 return [
-    'host'  =>  "",
-    'port'  =>  "",
-    'name'  =>  "app/database/unit_a.db",
-    'user'  =>  "",
-    'pass'  =>  "",
-    'type'  =>  "sqlite",
-    'prep'  =>  "1"
+    // 'host'  =>  "",
+    // 'port'  =>  "",
+    // 'name'  =>  "app/database/permission.db",
+    // 'user'  =>  "",
+    // 'pass'  =>  "",
+    // 'type'  =>  "sqlite",
+    // 'prep'  =>  "1"
+    'host' => "db",  # Nome do serviço no docker-compose
+    'port' => "5432",  # Porta padrão do PostgreSQL
+    'name' => "adianti",  # Nome do banco de dados
+    'user' => "admin",  # Usuário do banco
+    'pass' => "admin",  # Senha do banco
+    'type' => "pgsql",  # Tipo de banco de dados
+    'prep' => "1",  # Utilizar consultas preparadas
+    'slog' => "SystemSqlLog"  # Log SQL (opcional)
+
 ];
